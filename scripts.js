dbd4db9bb229ee2f744a7ee7468d1de3cfc6e1a1
Pietromuresu <muresupietro66@gmail.com>
2023-03-17 13:19:01 +0100

    First commit - ask name

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/*  Generare la password di un utente tramite i suoi dati 
+
+1. salvare il nome in una variabile
+2. salvare il cognome in una variabile
+3. salvare il suo colore preferito in una variabile 
+4. stampare in pagina una password composta da nome+cognome+colorepreferito+23
+
+*/
+
+const name = prompt('come ti chiami?');
+console.log(name);
